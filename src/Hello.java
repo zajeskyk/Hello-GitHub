cd531d14cc47b05a7865c694ecaef3614ccb5412
zajeskyk231 <zajeskyk231@ALB205-15.strose.edu>
2019-01-22 15:47:13 -0500

    edited a line

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Greeting Traveler!");
+		System.out.println("Greetings Traveler!");
+		System.out.println("I made an edit :)");
 	}
 }
